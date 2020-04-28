3646838b316f8dc8ca7c46c79a2430a2c843b9a5
Vasudha Varadarajan <earthling2304@gmail.com>
2020-04-28 06:06:43 +0000

    fixed crashing due to call by value

--- newicktojson.py
+++ newicktojson.py
@@ -1,33 +1,38 @@
 import newick
 import csv
+import gc
 
-def addTree(tree, jsonstring):
+jsonstring = ""
+line = 0;
+
+def addTree(tree):
+    global jsonstring;
+    gc.collect()
     jsonstring += "{name:"+ (tree.name[1:-12]) +",";
     jsonstring += "children:" 
     
     for subtree in tree.descendants:
-        jsonstring += addTree(subtree, jsonstring)
-    jsonstring += "}"
-    return jsonstring
+        gc.collect()
+        addTree(subtree)
+    jsonstring += "},"
 
-
-line = 0;
-with open('Languages.csv', mode='r', encoding="utf-8") as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:   
-        if line == 0:
-            line+=1
-            continue;
+csv_file = open('Languages.csv', mode='r', encoding="utf-8") 
+csv_reader = csv.DictReader(csv_file)
+for row in csv_reader:   
+    if line == 0:
+       line+=1
+       continue;
     
-        newick_string = (row['newick'].encode('ascii', 'ignore').decode("ascii"))
-        trees= newick.loads(newick_string)
-        jsonstring = ""
-        for tree in trees:
-            jsonstring += addTree(tree, jsonstring)
-        line+=1
-        if line == 242:
-            break; 
+    newick_string = (row['newick'].encode('ascii', 'ignore').decode("ascii"))
+    trees= newick.loads(newick_string)
+    for tree in trees:
+       addTree(tree)
+    line+=1
+    if line == 242:
+      break; 
         #jsonstr = "";
         #jsonstr = addTree(trees, jsonstr);
-
-        print(jsonstring)
+csv_file.close()
+filewrite = open(something.json, mode="w")
+filewrite.write(jsonstring)
+filewrite.close()
